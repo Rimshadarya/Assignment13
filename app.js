f53d84ccd5d09daf93279f01737106716c9c84e2
Rimshadarya <rimshadarya@gmail.com>
2024-09-09 15:11:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,282 @@
+// chapter 1{ALert}
+
+// Question 1
+
+// alert("Hello")
+
+// Question 2
+
+//  alert(" JavaScript Alert  \n \n Error! Please enter a valid password")
+
+//  Question 3
+
+// alert(" JavaScript Alert \n \n Welcome to JS land  \n Happy coding")
+
+// Question 4 part a
+
+// alert(" JavaScript Alert \n \n Welcome to JS land")  
+
+// Question 4 part b
+
+// alert(" JavaScript Alert \n \n Happy Coding! \n Prevent this page from creating additional dialogs ") 
+
+// Question 5
+
+// var name =(" JavaScript Alert \n \n \n")
+
+// var name2 =("Hello...! I can run JS through my web browsers console ")
+
+// console.log(name + name2)
+
+// Question 6
+
+// alert("Hello")
+
+// CHAPTER 2{Variable for string}
+
+
+// Question 1
+
+
+// var username 
+
+// Question 2
+
+// var myName = ("RIMSHA QASIM")
+
+// console.log(myName)
+
+// Question 3
+
+//  var message = ("Hello World")
+
+//  document.write(message)
+
+//  alert("Hello World")
+
+//  Question 4
+
+// alert("Jhon doe")
+
+// alert("15 years old")
+
+// alert("Certifies App develoment")
+
+// Question 5
+
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P ")
+
+// Question 6
+
+// var email = ("my email is example@example.com")
+
+// alert(email)
+
+// Question7
+
+// var book = "I am trying to learn from the Book A smarter \n way to learn JavaScript”."
+
+// alert(book)
+
+// Question8
+
+// var message = ("Yah! I can write HTML content through JavaScript.")
+
+// document.write(message)
+
+// Question9
+
+
+// Chapter3 {Variable for numbers}
+
+
+// Question1
+
+// var age = "I am 18 years old"
+
+// alert(age)
+
+// Question2
+
+// var numbers = "You have visited this website 5times."
+
+// alert(numbers)
+
+// Question3
+
+// var birthYear = "My birth year is 2006"
+
+// document.write(birthYear)
+
+// Question4
+
+// var details = "Ms.Abeeha noor ordered 2Long Shirts With 2Capri from J."
+
+// document.write(details)
+
+
+// Chapter4{Legal And Illegal}
+
+
+// Question1
+
+// var num = "10", name = "Rimsha", age = "18 years"
+
+// Question2
+
+// legal variable
+
+// var _name = ""
+// var $age  = ""
+// var _city = ""
+// var $country = ""
+// var _education = ""
+
+// console.log($age)
+// console.log(_name)
+// console.log(_city)
+// console.log(_education)
+// console.log($country)
+
+
+// Illegal variable
+
+// var !name = ""
+// var @age = ""
+// var %city = ""
+// var %country = ""
+// var &_education = ""
+
+// console.log(!name)
+// console.log(@age)
+// console.log(%city)
+// console.log(%country)
+// console.log(&_education)
+
+// Question3
+
+// var $case = "RULES FOR NAMING JS VARIABLES"
+
+// document.write($case)
+
+// var data = "Variables names can only contain ALPHABETS, NUMERIC, SPECIAL CHARACTERS AND UNDERSCORES. For Example: my_1st variable"
+
+// document.write(data)
+
+// var work = "Variables must begin with a lowercase letter, uppercase letter or underscores."
+
+// document.write(work)
+
+// var ele = "Variables are case SENSITIVE" 
+
+// document.write(ele)
+
+// var key = "Variables name should not be JS keywords"
+
+// document.write(key)
+
+
+// CHAPTER1To3
+
+
+// Question1
+
+// var firstName = "Rimsha"
+
+// alert(firstName)
+
+// var lastName = "Muhammad Qasim"
+
+// alert(lastName)
+
+// var Email = "abc123@gmail.com"
+
+// alert(Email)
+
+// var password = "12345adsf"
+
+// alert(password)
+
+// var phoneNumber = "03XX-XXXXXXX"
+
+// alert(phoneNumber)
+
+// Question2
+
+// alert("alert! You're learning JavaScript!")
+
+// Question3
+
+// alert("Thanks For Visiting Our Website..")
+
+
+// Chapter2(Variable for string)
+
+
+// Question1
+
+// var dateOfBirth = 
+
+// console.log(dateOfBirth)
+
+// Question2
+
+// var carName = ""
+
+// console.log(carName)
+
+// var carName = "Lamborghini"
+
+// console.log(carName)
+
+// Question3
+
+// var teamName = "The Achievers"
+
+// alert(teamName)
+
+// Question4
+
+// var type_bestman = "Charlie"
+
+// console.log(type_bestman)
+
+
+// chapter3(Variables For Number)
+
+
+// Question1
+
+// var caseQty = 14
+
+// console.log(caseQty)
+
+// Question2
+
+// var num = "9"
+
+// console.log(num)
+
+// Question3
+
+var num = 13;
+
+var num2 = 17;
+
+console.log(num + num2)
+
+// Question4
+
+// var merchTotal = 100;
+
+// var shippingCharge = 10;
+
+// console.log(merchTotal + shippingCharge);
+
+// Question5
+
+// var num = 6
+
+// var num2 = 52
+
+// console.log(6 + 52)
